d5376e3e1f653dd22e50ec980f323f155b70ba92
isanben10 <ivannssanben@gmail.com>
2025-12-10 13:13:19 +0100

    Subir Portafolio
    pagina web con donde voy a ir añadiendo mi portafolios

---
+++ PORTAFOLIO/script.js
@@ -0,0 +1,17 @@
+//  animaciones al hacer scroll
+const elements = document.querySelectorAll('.fade-slide');
+
+function checkVisibility() {
+    const trigger = window.innerHeight * 0.85;
+
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+
+        if (top < trigger) {
+            el.classList.add('visible');
+        }
+    });
+}
+
+window.addEventListener('scroll', checkVisibility);
+window.addEventListener('load', checkVisibility);
